b7d5d04ed9d3de18a7a4accfa2f2746c47f53795
Costin Draghici <draghicicostin20@stud.ase.ro>
2023-11-25 22:02:49 +0200

    Added date class header and cpp and tested it in main

---
+++ CostinDraghici_OOP/main.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+#include <string>
+#include "date.h"
+
+using namespace std;
+
+//TODO:
+// today: implement location class
+// today++: implement ticket class
+
+int main() {
+	Date a(48, 6, 21, Month::AUGUST, 2022);
+	cout << a << endl << endl;
+}
